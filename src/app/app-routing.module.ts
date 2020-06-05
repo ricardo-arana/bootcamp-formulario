210721c0bad1b7d982681adf859cf6b8cd56abb9
Ricardo Arana Reyes <raranare@everis.com>
2020-06-05 15:54:52 -0500

    version inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { DatosComponent } from './pages/datos/datos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'template', component: TemplateComponent},
+  {path: 'datos', component: DatosComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { DatosComponent } from './pages/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TemplateComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule,
